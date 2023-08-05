4c2ac15ad6a52fa4937e8755561f22218e64d64b
Javier <Jvasquez5422@pelhamprepacademy.org>
2023-08-04 21:32:05 -0400

    Began working

---
+++ script.js
@@ -0,0 +1,39 @@
+let pbutton = document.getElementById("p-button");
+let pdiv = document.getElementById("p-div");
+
+let Imgbutton = document.getElementById("img-button");
+let Imgiv = document.getElementById("img-div");
+
+
+pbutton.onclick = function(){
+    pdiv.innerHTML = "Hot chocolate is bad today";
+    pdiv.style["margin-top"] = "5px";
+    pdiv.style.height = "20px";
+    pdiv.style["background-color"] = "black";
+    pdiv.style["color"] = "white";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
